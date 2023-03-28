55c83c38314cbc6ba36d23ff4799f975219757e6
ayano1011 <panda.mrlst.80@gmail.com>
2023-03-29 06:53:25 +0900

    url変更

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 liff.init({ liffId: "1660795044-e5Ox5zMj" })
-.then(() => {
-    if (!liff.isLoggedIn()) {
-        liff.login();
-    } else {
-        document.getElementById('title').textContent = 'ログイン済み';
-    }
-})
+    .then(() => {
+        if (!liff.isLoggedIn()) {
+            liff.login();
+        } else {
+            document.getElementById('title').textContent = '';
+            liff.openWindow({
+                url: 'https://ayano1011.github.io/rabbit-chart/',
+                external: false,
+            });
+        }
+
+    })
